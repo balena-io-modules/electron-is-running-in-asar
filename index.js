9b28aa0f10da39b28fed830edd069bdf243c7abd
Juan Cruz Viotti <jv@jviotti.com>
2018-10-30 18:11:33 +0000

    Update owner
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2016 Juan Cruz Viotti. https://github.com/jviotti
+ * Copyright (c) 2016 Balena.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
